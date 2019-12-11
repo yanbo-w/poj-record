5be2c980e913fe5e235be14075a1c3243ed5724d
Yanbo Wang <37865267+yanbo-w@users.noreply.github.com>
2019-12-11 20:15:19 +0800

    tst

--- tst.cpp
+++ tst.cpp
@@ -8,64 +8,49 @@ using namespace std;
 
 class Solution {
 public:
-    int movingCount(int threshold, int rows, int cols)
-    {
-        if(rows == 0 || cols == 0 || threshold < 0) return 0;
-        vector<vector<int> > map(rows);
-        for(int i = 0; i < rows; i++) map[i].resize(cols);
-        queue<int> x;
-        queue<int> y;
-        x.push(0);
-        y.push(0);
-        int res = 1;
-        map[0][0] = 1;
-        while(!x.empty()){
-            if(move(x.front()+1, y.front(), rows, cols, map, threshold)){
-                map[x.front()+1][y.front()] = 1;
-                x.push(x.front()+1);
-                y.push(y.front());
-                res++;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        if(nums.size() < 3) return res;   
+        qsort(nums, 0, nums.size()-1);
+        for(int i = 0; i < nums.size(); i++){
+            if(i == 0 || nums[i] != nums[i-1]){
+                int lo = i+1;
+                int hi = nums.size()-1;
+                while(lo < hi){
+                    if(nums[lo] + nums[hi] < -nums[i]) lo++;
+                    else if(nums[lo] + nums[hi] > -nums[i]) hi--;
+                    else if(nums[lo] + nums[hi] == -nums[i]){
+                        vector<int> now;
+                        now.push_back(nums[i]);
+                        now.push_back(nums[lo]);
+                        now.push_back(nums[hi]);
+                        res.push_back(now);
+                        lo++;
+                        hi--;
+                        while(lo < hi && nums[lo] == nums[lo-1]) lo++;
+                        while(lo < hi && nums[hi] == nums[hi+1]) hi--;
+                    }
+                }    
             }
-            if(move(x.front()-1, y.front(), rows, cols, map, threshold)){
-                map[x.front()-1][y.front()] = 1;
-                x.push(x.front()-1);
-                y.push(y.front());
-                res++;
-            }
-            if(move(x.front(), y.front()+1, rows, cols, map, threshold)){
-                map[x.front()][y.front()+1] = 1;
-                x.push(x.front());
-                y.push(y.front()+1);
-                res++;
-            }
-            if(move(x.front(), y.front()-1, rows, cols, map, threshold)){
-                map[x.front()][y.front()-1] = 1;
-                x.push(x.front());
-                y.push(y.front()-1);
-                res++;
-            }
-            x.pop();
-            y.pop();
         }
         return res;
     }
 private:
-    bool move(int x, int y, int rows, int cols, vector<vector<int> > &map, int threshold){
-        if(x >= 0 && y >= 0 && x < rows && y < cols && map[x][y] == 0 && threshlimit(x, y, threshold)) return true;
-        else return false;
-    }
-    bool threshlimit(int i, int j, int threshold){
-        int sum = 0;
-        while(i > 0){
-            sum += i%10;
-            i /= 10;
+    void qsort(vector<int> &nums, int lo, int hi){
+        if(nums.size() > 0 && lo < hi){
+            int lo_init = lo;
+            int hi_init = hi;
+            int partion = nums[lo];
+            while(lo < hi){
+                while(lo < hi && nums[hi] >= partion) hi--;
+                nums[lo] = nums[hi];
+                while(lo < hi && nums[lo] <= partion) lo++;
+                nums[hi] = nums[lo];
+            }
+            nums[lo] = partion;
+            qsort(nums, lo_init, lo-1);
+            qsort(nums, lo+1, hi_init);
         }
-        while(j > 0){
-            sum += j%10;
-            j /= 10;
-        }
-        if(sum <= threshold) return true;
-        else return false;
     }
 };
 
@@ -73,7 +58,10 @@ int main(){
 
     Solution t;
 
-    cout << t.movingCount(15,1,1) << endl;
+    vector<int> nums{-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6};
+
+    vector<vector<int>> res =  t.threeSum(nums);
+
     // cout << "----" <<(getnum() < getnum() ? getnum() : getnum()) << "----" << endl;
 
     return 0;
